6a718896fb998710360b3af258004ebe5043fbfd
yubathom <yubathom@gmail.com>
2020-04-06 12:13:48 -0300

    chore: ajusta build em travis renomeando arquivos

---
+++ app/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+	<footer>
+		<div>
+			<span>Dúvidas e sugestões</span> &middot; Ramal 7533 &middot; desenvolvimento@spurbanismo.prefeitura.sp.gov.br
+		</div>
+	</footer>
+</template>
+
+<script>
+export default {
+	name: 'Footer'
+}
+</script>
+
+<style lang="scss">
+footer {
+	background-color: $verde;
+	color: #FFF;
+	padding: 2.5rem 0;
+	margin: 6rem 0 0;
+	div {
+		max-width: 1200px;
+		margin: 0 auto;
+		span { font-weight: bold; }
+	}
+}
+</style>
