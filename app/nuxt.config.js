eeebd3e123eb4d0b2d00ebfce301c8524509f239
yubathom <yubathom@gmail.com>
2020-04-06 13:16:15 -0300

    chore(ci): build condicional

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -2,12 +2,15 @@ const repoName = (process.env.TRAVIS_REPO_SLUG || '').split('/').pop()
 
 export default {
   router: {
-    base: process.env.CI ? repoName : '/' // https://spurb.github.io/relatorios/
+    base: process.env.CI ? repoName : '/' // https://spurb.github.io/apropriacaohoras/
   },
   mode: 'spa',
   modules: ['@nuxtjs/style-resources'],
   styleResources: {
     scss: ['@/assets/style/variables.scss', '@/assets/style/theme.scss']
   },
-  css: ['@/assets/style/icones/participe.css']
+  css: ['@/assets/style/icones/participe.css'],
+  generate: {
+    dir: process.env.CI ? 'gh-pages' : 'dist'
+  }
 }
--- build.app.sh
+++ build.app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd app
 npm install
-npx nuxt generate
+npm run generate
