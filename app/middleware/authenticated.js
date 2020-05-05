6450ec5dc6390bf0de6886f3420f7a4a99e89ce4
m-dantas <mauriciodantas.contato01@bol.com.br>
2020-05-05 20:16:33 -0300

    feat(app): add vuex-persist para manter o user logado

--- app/middleware/authenticated.js
+++ app/middleware/authenticated.js
@@ -1,5 +1,11 @@
 export default async ({ store, route }) => {
-	if (!store.state.usuario.id && route.path !== '/login') {
-		store.app.router.replace('/login')
-	}
+  const credencial = JSON.parse(localStorage.getItem('credencial-user'))
+
+  if (!credencial) {
+    store.app.router.replace('/login')
+  } else {
+    const token = credencial.usuario.token
+    if (token === "" && route.path !== '/login') store.app.router.replace('/login')
+    if (token !== "" && route.path === '/login') store.app.router.replace('/registrar')
+  } 
 }
