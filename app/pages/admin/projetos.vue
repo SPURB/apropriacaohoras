8867256d92f0dfa89c5b4aa1a38e5e9875dabf60
yubathom <yubathom@gmail.com>
2020-05-12 19:58:34 -0300

    feat(app): sincronizacao de roteamento com requisicoes

--- app/pages/admin/projetos.vue
+++ app/pages/admin/projetos.vue
@@ -2,56 +2,118 @@
   <div class="projetos">
     <div class="row">
       <div class="column">
-        <h2>Projetos</h2>
-				<list-horizontal-nav :routes='projetosRoutes' />
-        <!-- <ul v-if='!idProjeto' class="lista-projetos">
-          <li
-            class="lista-projetos__projeto"
-            v-for="(projeto, index) in projetos"
-            :key="index"
-          >
-            <router-link :to="{ query: { projeto: projeto.id } }" tag="a">{{
-              projeto.nome
-            }}</router-link>
-          </li>
-        </ul>
-				<admin-projetos-projeto v-else :idProjeto="idProjeto" /> -->
+        <list-horizontal-nav
+          v-if="currentStep === 'Projetos'"
+          :routes="projetosRoutes"
+          :action-type="'go'"
+        />
+        <list-horizontal-nav
+          v-if="currentStep === 'Fases'"
+          :routes="fasesRoutes"
+          :action-type="'go'"
+        />
+        <list-horizontal-nav
+          v-if="currentStep === 'Subatividades'"
+          :routes="subatividadesRoutes"
+          :action-type="'close'"
+        />
       </div>
     </div>
   </div>
 </template>
 <script>
-import { mapGetters, mapActions } from 'vuex'
-// import AdminProjetosProjeto from '~/components/sections/AdminProjetosProjeto'
+import { mapState, mapGetters, mapActions } from 'vuex'
 import ListHorizontalNav from '~/components/router-links/ListHorizontalNav'
 export default {
   name: 'Projetos',
-	layout: 'admin',
-	components: {
-		// AdminProjetosProjeto,
-		ListHorizontalNav
-	},
+  layout: 'admin',
+  components: {
+    ListHorizontalNav
+  },
   computed: {
-		...mapGetters('admin/equipes', ['projetos']),
-		projetosRoutes () {
-			return this.projetos.map(projeto => {
-				return {
-					to: { query: { projeto: projeto.id } },
-					title: projeto.nome
-				}
-			})
-		},
-		idProjeto () {
-			const id =  this.$route.query.projeto
-			return id ? id : 0
-		}
+    ...mapGetters('admin/equipes', ['projetos']),
+    ...mapGetters('admin/projetos', ['fases']),
+    ...mapState('admin/projetos', ['subatividades']),
+    projetosRoutes () {
+      return this.projetos.map(projeto => {
+        return {
+          to: { query: { projeto: projeto.id } },
+          title: projeto.nome
+        }
+      })
+    },
+    fasesRoutes () {
+      if (!this.fases.length) {
+        return []
+      }
+      return this.fases.map(fase => {
+        return {
+          to: {
+            query: {
+              projeto: this.idProjeto,
+              fase: fase.id
+            }
+          },
+          title: fase.nome
+        }
+      })
+    },
+    subatividadesRoutes () {
+      if (!this.subatividades.length) {
+        return []
+      }
+      return this.subatividades.map(subatividade => {
+        return {
+          to: {
+            query: {
+              projeto: this.idProjeto,
+              fase: this.IdFase,
+              subatividade: subatividade.id
+            }
+          },
+          title: subatividade.nome
+        }
+      })
+    },
+    idProjeto () {
+      const id = this.$route.query.projeto
+      return id ? id : 0
+    },
+    idFase () {
+      const id = this.$route.query.fase
+      return id ? id : 0
+    },
+    selectedFase () {
+      return this.$route.query.fase
+    },
+    currentStep () {
+      if (this.idProjeto && !this.idFase) {
+        return 'Fases'
+      } else if (this.idProjeto && this.idFase) {
+        return 'Subatividades'
+      } else {
+        return 'Projetos'
+      }
+    }
+  },
+  watch: {
+    selectedFase (id) {
+      if (id) {
+        this.getSubatividades(id)
+      }
+    }
   },
   methods: {
-    ...mapActions('admin/equipes', ['getProjetos'])
+    ...mapActions('admin/equipes', ['getProjetos']),
+    ...mapActions('admin/projetos', ['getFases', 'getSubatividades'])
   },
   created () {
     if (!this.projetos.length) {
       this.getProjetos()
+    }
+
+    if (!this.fases.length) {
+      this.getFases()
     }
   }
 }
--- app/store/admin/projetos.js
+++ app/store/admin/projetos.js
@@ -1,12 +1,58 @@
+import Fases from '@/services/api-fase'
+import Subatividades from  '@/services/api-subatividade'
 export const state = () => ({
-	fases: []
+	fetching: false,
+	error: false,
+	message: '',
+	fases: [],
+	subatividades: []
 })
 
 export const getters = {
+	fases: (state, getters, rootState) => {
+		const fetchedTables = rootState['form-registrar-horas'].dataSelects
+
+		if (fetchedTables.length) {
+			return fetchedTables.find(select => select.title === 'Fases')
+		}
+		else {
+			return state.fases
+		}
+	}
 }
 
 export const actions = {
+	getFases: ({ commit }) => {
+		commit('IS_FETCHING', true)
+		Fases.get()
+			.then(({ data }) => {
+				commit('SET', { data, stateKey: 'fases' })
+				commit('SET_ERROR', { status: false, message: '' })
+			})
+			.catch(({ message }) => {
+				commit('SET_ERROR', { status: true, message })
+			})
+			.finally(() => commit('IS_FETCHING', false))
+	},
+	getSubatividades: ({ commit }, idFase) => {
+		commit('IS_FETCHING', true)
+		Subatividades.get(idFase)
+			.then(({ data }) => {
+				commit('SET', { data, stateKey: 'subatividades' })
+				commit('SET_ERROR', false)
+			})
+			.catch(({ message }) => {
+				commit('SET_ERROR', { status: true, message })
+			})
+			.finally(() => commit('IS_FETCHING', false))
+	}
 }
 
 export const mutations = {
+	SET: (state, { data, stateKey }) => { state[stateKey] = data.values },
+	SET_ERROR: (state, { status, message })=> {
+		state.error = status
+		state.message = message
+	},
+	IS_FETCHING: (state, fetchState) => { state.fetching = fetchState }
 }
