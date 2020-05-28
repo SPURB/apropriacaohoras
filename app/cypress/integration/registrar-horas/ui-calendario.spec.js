f8c9fed7d886de694eebd2b353f990d12982bd72
m-dantas <mauriciodantas.contato01@bol.com.br>
2020-05-28 15:45:35 -0300

    refactor(app): remove repetição de código para login

--- app/cypress/integration/registrar-horas/ui-calendario.spec.js
+++ app/cypress/integration/registrar-horas/ui-calendario.spec.js
@@ -1,68 +1,23 @@
 describe('Teste unitário para o componente calendário', () => {
-  
-  it('Testar avançar e voltar mês', () => {
-    cy.visit('/login')
+  beforeEach(() => {
+    cy.login('/registrar')
+  })
 
-    // seta email
-    cy.get('[data-cy=input__email]').click()
-    cy.get('[data-cy=input__email').type('mgiannoni')
-
-    cy.get('[data-cy=btn__email]').click()
-
-    // seta password
-    cy.get('[data-cy=input__pass]').click()
-    cy.get('[data-cy=input__pass]').type('123456')
-
-    // confirma o acesso
-    cy.get('[data-cy=btn__confirm]').click()
-    cy.wait(1000)
-    
+  it('Testar avançar e voltar mês', () => {  
     cy.get('[data-cy=prox__mes]').click()
     cy.wait(1000)
 
     cy.get('[data-cy=prev__mes]').click()
   })
 
-  it('Testar multiplas seleções de datas', () => {
-    cy.visit('/login')
-
-    // seta email
-    cy.get('[data-cy=input__email]').click()
-    cy.get('[data-cy=input__email').type('mgiannoni')
-
-    cy.get('[data-cy=btn__email]').click()
-
-    // seta password
-    cy.get('[data-cy=input__pass]').click()
-    cy.get('[data-cy=input__pass]').type('123456')
-
-    // confirma o acesso
-    cy.get('[data-cy=btn__confirm]').click()
-    cy.wait(1000)
-    
+  it('Testar multiplas seleções de datas', () => {    
     cy.get('[data-cy=select__data]').then(btn => {
       btn[15].click()
       btn[16].click()
     })
   })
 
-  it('Testar multiplas seleções de datas e remoção de seleção', () => {
-    cy.visit('/login')
-
-    // seta email
-    cy.get('[data-cy=input__email]').click()
-    cy.get('[data-cy=input__email').type('mgiannoni')
-
-    cy.get('[data-cy=btn__email]').click()
-
-    // seta password
-    cy.get('[data-cy=input__pass]').click()
-    cy.get('[data-cy=input__pass]').type('123456')
-
-    // confirma o acesso
-    cy.get('[data-cy=btn__confirm]').click()
-    cy.wait(1000)
-    
+  it('Testar multiplas seleções de datas e remoção de seleção', () => {    
     cy.get('[data-cy=select__data]').then(btn => {
       btn[15].click()
       btn[16].click()
@@ -77,22 +32,6 @@ describe('Teste unitário para o componente calendário', () => {
   })
 
   it('Testar registro com multiplas datas no mesmo MÊS', () => {
-    cy.visit('/login')
-
-    // seta email
-    cy.get('[data-cy=input__email]').click()
-    cy.get('[data-cy=input__email').type('mgiannoni')
-
-    cy.get('[data-cy=btn__email]').click()
-
-    // seta password
-    cy.get('[data-cy=input__pass]').click()
-    cy.get('[data-cy=input__pass]').type('123456')
-
-    // confirma o acesso
-    cy.get('[data-cy=btn__confirm]').click()
-    cy.wait(1000)
-
     cy.get('[data-cy=select__data]').then(btn => {
       btn[15].click()
       btn[16].click()
@@ -129,22 +68,6 @@ describe('Teste unitário para o componente calendário', () => {
   })
 
   it('Testar registro com multiplas datas em meses DIFERENTES', () => {
-    cy.visit('/login')
-
-    // seta email
-    cy.get('[data-cy=input__email]').click()
-    cy.get('[data-cy=input__email').type('mgiannoni')
-
-    cy.get('[data-cy=btn__email]').click()
-
-    // seta password
-    cy.get('[data-cy=input__pass]').click()
-    cy.get('[data-cy=input__pass]').type('123456')
-
-    // confirma o acesso
-    cy.get('[data-cy=btn__confirm]').click()
-    cy.wait(1000)
-
     cy.get('[data-cy=select__data]').then(btn => {
       btn[15].click()
       btn[16].click()
@@ -184,7 +107,6 @@ describe('Teste unitário para o componente calendário', () => {
     cy.get('[data-cy=registrar__horas]').click()
     cy.wait(1500)
 
-    cy.get('[data-cy=modal__message]').contains('Horas cadastradas')
-    
+    cy.get('[data-cy=modal__message]').contains('Horas cadastradas')   
   })
 })
