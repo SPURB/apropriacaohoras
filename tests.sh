f8cfb5548a5de159139a13903199b7e0fc553ef6
yubathom <yubathom@gmail.com>
2020-06-02 13:48:32 -0300

    test(app): testa build de nuxt generate invés de nuxt dev

--- tests.sh
+++ tests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-	cd app
-	npm install
-	npm run test:e2e
+  cd app
+  npm run test:e2e:ci
 else
- echo 'this is not a pull request branch. Skipping app end-to-end tests...'
+  echo 'this is not a pull request branch. Skipping app end-to-end tests...'
 fi
