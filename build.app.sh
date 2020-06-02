8fb21fb10aa50959e7458414c778bc8ef9db63e0
yubathom <yubathom@gmail.com>
2020-06-02 13:18:16 -0300

    chore(build): inclui install em build de app

--- build.app.sh
+++ build.app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "dev/app" ]; then
 	cd app
+	npm install
 	npm run generate
 else
  echo 'this is not master branch or dev/app branch. Skipping app build test...'
