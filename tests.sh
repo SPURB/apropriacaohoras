ceb40870a17dea506048458f0425d4bb2d3a1a29
yubathom <yubathom@gmail.com>
2020-06-02 13:02:48 -0300

    chore(api): ajuste na ordem de instalação do app

--- tests.sh
+++ tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 	cd app
+	npm install
 	npm run test:e2e
 else
  echo 'this is not a pull request branch. Skipping app end-to-end tests...'
