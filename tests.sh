81a11cba355eaf78a663d78d7b9624c7c7513382
yubathom <yubathom@gmail.com>
2020-06-02 17:49:23 -0300

    chore(wip): wip

--- tests.sh
+++ tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   cd app
-  npm run build
   npm run test
 else
   echo 'this is not a pull request branch. Skipping app end-to-end tests...'
