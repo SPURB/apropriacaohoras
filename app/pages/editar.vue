d0969a923eae1c1a92c14462d463bbaca9705aaf
m-dantas <mauriciodantas.contato01@bol.com.br>
2020-06-05 16:56:20 -0300

    feat(app): agrupando por projeto

--- app/pages/editar.vue
+++ app/pages/editar.vue
@@ -13,13 +13,15 @@
 import AppHeader from '~/components/AppHeader'
 import ListarHoras from '~/components/ListarHoras'
 import Horas from '~/services/api-horas'
+import Subatividade from '~/services/api-subatividade'
 import Lib from '~/libs/'
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 export default {
-  name: 'Editar',
+  name: 'EditarHoras',
   data () {
     return {
-      arrayHoras: []
+      arrayHoras: [],
+      subatividade: []
     }
   },
   components: {
@@ -29,6 +31,10 @@ export default {
     ...mapState('usuario', {
       id: state => state.id
     }),
+    ...mapState('form-registrar-horas', {
+      projetos: state => state.dataSelects[1].values,
+      fases: state => state.dataSelects[0].values
+    }),
     formateDate () {
       const date = Lib.splitDate(this.$route.query.data)
       return `${date.sDay}/${date.sMonth}/${date.sYear}`
@@ -36,17 +42,56 @@ export default {
     url () {
       const data = this.$route.query.data
       return `?usuario=${this.id}&dataRefInicio=${data}`
-    },
-    countHoras () {}
+    }
+  },
+  watch: {
+    arrayHoras () {
+      this.registro()
+    }
   },
   created () {
-    this.getHoras()
+    this.getHorasSubatividades()
+    this.addData() // adiciona no vuex as fases/projetos
   },
   methods: {
-    async getHoras () {
-      await Horas.get(this.url).then(res => {
-        this.arrayHoras = res.data.values
+    ...mapActions('form-registrar-horas', ['addData']),
+    async getHorasSubatividades () {
+      await Promise.all([Horas.get(this.url), Subatividade.get()])
+        .then(res => {
+          this.arrayHoras = res[0].data.values
+          this.subatividade = res[1].data.values
+        })
+        .catch(err => {
+          alert('Ocorreu algum erro! Tente mais tarde.')
+          new Error(err)
+        })
+    },
+    group_by (lista, coluna) {
+      let colunas = {}
+      let resultado = []
+
+      lista.forEach(function (item) {
+        let reg = {}
+        colunas[item[coluna]] = colunas[item[coluna]] || []
+
+        for (let i in item) if (i != coluna) reg[i] = item[i]
+
+        colunas[item[coluna]].push(reg)
       })
+
+      for (let i in colunas) {
+        const projeto = {
+          key: this.projetos.find(projeto => projeto.id === +i).nome,
+          values: colunas[i]
+        }
+        resultado.push(projeto)
+      }
+
+      return resultado
+    },
+    registro () {
+      const agrupados = this.group_by(this.arrayHoras, 'projeto')
+      console.log(agrupados)
     }
   }
 }
