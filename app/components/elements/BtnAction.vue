58a9d2b39217a1706cca74a872285d6b3c0c8457
yubathom <yubathom@gmail.com>
2020-06-23 22:29:23 -0300

    feat(app): define logica de roteamento

---
+++ app/components/elements/BtnAction.vue
@@ -0,0 +1,106 @@
+<template>
+  <button
+    class="btn-action"
+    :class="{
+      'btn-action--danger': danger,
+      'btn-action--compact': compact,
+      'btn-action--loading': loading
+    }"
+    :disabled="reallyDisabled"
+    @click="action"
+  >
+    <template v-if="loading">Carregando</template>
+    <template v-else>{{ title }}</template>
+  </button>
+</template>
+<script>
+/*
+  exemplos de uso (no pai)
+  <btn-action title="default" @action="anyMethod"/>
+  <btn-action title="Cancelar" @action="anyMethod" danger />
+  <btn-action title="Compacto" @action="anyMethod" compact />
+  <btn-action title="Fetching" @action="anyMethod" :loading="someComputed"/>
+  <btn-action title="Desabilitado" @action="anyMethod('aoa')" :disabled="someComputed" />
+*/
+export default {
+  name: 'BtnAction',
+  computed: {
+    reallyDisabled () {
+      return this.loading || this.disabled
+    }
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    danger: {
+      type: Boolean,
+      default: false
+    },
+    compact: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    action (event) {
+      this.$emit('action', event)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.btn-action {
+  border: 2px solid rgba(255, 255, 255, 0.24);
+  background-color: $verde;
+  font-size: 1rem;
+  font-family: $grot;
+  border-radius: 2rem;
+  color: #fff;
+  padding: 1rem 1.62rem;
+  margin: 2rem 0 0;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.48);
+  transition: all ease-in 0.2s;
+  &--danger {
+    background-color: $vermelho;
+  }
+  &--compact {
+    padding: 0.62rem;
+    font-size: small;
+  }
+  &--loading {
+    animation: pulse 1s infinite alternate;
+  }
+  &:hover {
+    cursor: pointer;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.24);
+  }
+  &--loading,
+  &[disabled] {
+    background-color: $cinza1;
+  }
+  &[disabled]:hover {
+    cursor: not-allowed;
+  }
+  &--loading[disabled]:hover {
+    cursor: wait;
+  }
+}
+@keyframes pulse {
+  0% {
+    opacity: 0.7;
+  }
+  100% {
+    opacity: 0.9;
+  }
+}
+</style>
---
+++ app/pages/pre-impressao.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="pre-impressao">
+    <div class="pre-impressao__container">
+      <voltar class="pre-impressao__voltar" :to="from" />
+
+      <div class="row">
+        <div class="column">
+          a4
+        </div>
+      </div>
+      <div class="row pre-impressao__btns">
+        <div class="column column--left">
+          <btn-action title="Gerar csv" @action="createCsv" />
+        </div>
+        <div class="column column--center">
+          <btn-action title="Gerar pdf" @action="createPdf" />
+        </div>
+        <div class="column column--right">
+          <btn-action title="Cancelar" @action="goBack(from)" danger />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Voltar from '~/components/router-links/Voltar'
+import BtnAction from '~/components/elements/BtnAction'
+export default {
+  name: 'PreImpressao',
+  components: {
+    Voltar,
+    BtnAction
+  },
+  computed: {
+    from () {
+      const from = this.$route.query.from
+      return from ? from : '/'
+    }
+  },
+  methods: {
+    createCsv () {
+      console.log('gerar csv')
+    },
+    createPdf () {
+      console.log('gerar pdf')
+    },
+    goBack (route) {
+      this.$router.push(route)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+body {
+  background-color: #fff;
+}
+.pre-impressao {
+  background-color: #fff;
+  min-height: 100vh;
+  &__container {
+    max-width: $desktop;
+    margin: auto;
+    padding-top: 3rem;
+  }
+  &__voltar {
+    width: 100%;
+  }
+  &__btns {
+    max-width: 600px;
+    margin: auto;
+  }
+}
+
+.column {
+  &--center {
+    margin: 0 1rem;
+  }
+}
+</style>
