0b93f07a454266e5b390e8f11c07d6d08289ddf6
Maurício Dantas <45770805+m-dantas@users.noreply.github.com>
2020-07-10 15:27:32 -0300

    feat(api): template de email de cadastro (#91)
    * feat(api): inclui html para o email
    
    * feat(api): inclui express.static para arquivos no projeto
    
    * chore(api): remove url de send email
    
    * chore(api): inclui mudanças do packjage-lock.json
    
    * chore(api): change para path dinamico na src da imagem

--- api/src/controllers/acoes.js
+++ api/src/controllers/acoes.js
@@ -2,6 +2,8 @@ import db from '../models'
 import sequelize from 'sequelize'
 import fetch from 'node-fetch'
 
+const logo = `/static/spurbanismo-mono-negativo.png`
+
 module.exports = {
   horas: {
     totalPorPeriodo: (inicio, fim, idProjeto) => {
@@ -44,14 +46,100 @@ module.exports = {
   },
 
   usuarios: {
-    criarMensagem: (nome, email, senha) => {
+    criarMensagem: (nome, email, senha, host) => {
       return `
-        <h3>Olá ${nome},</h3>
-        <p>A sua senha do sistema de registro de horas é:</p>
-        <p><strong>${senha}</strong></p>
-        <p>Faça o login para acessar o sistema: <a href="https://servicos.spurbanismo.sp.gov.br/apropriacaohoras/?email=${email}">login</a></p>
-        <p>Se preferir defina uma nova senha pelo link abaixo:</p>
-        <p><a href="https://servicos.spurbanismo.sp.gov.br/apropriacaohoras/reset?email=${email}&reset=${senha}">Redefinir senha</a></p>
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>  
+      </head>
+      <body>
+        <table width="640" cellpadding="0" cellspacing="0" border="0" class="wrapper" bgcolor="#FFFFFF"
+          style="width:100%;font-family:roboto, sans-serif;padding:0;Margin:0">
+          <tr>
+            <td height="10" style="font-size:10px; line-height:10px;">&nbsp;</td>
+          </tr>
+          <tr>
+            <td align="center" valign="top">
+              <table width="600" cellpadding="15" cellspacing="0" border="0" class="container">
+                <tr>
+                  <td align="center" valign="top" bgcolor="#048874">
+                    <img src="${host + logo}" width="200" height="50" style="margin:0; 
+                          padding:0; 
+                          border: none; 
+                          display:block;" border="0" alt="" />
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <td align="center" valign="top">
+              <table width="600" cellpadding="0" cellspacing="30" border="0">
+                <tr>
+                  <td bgcolor="#FFFFFF">
+                    Olá
+                    <strong>${nome}</strong>
+                    você recebeu acesso ao sistema de registros de horas.
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <td align="center">
+              <table width="600" cellpadding="0" cellspacing="0" border="0" class="container">
+                <tr>
+                  <td align="center">
+                    <p>Sua senha de acesso ao sistema é:</p>
+                    <h3>
+                      <strong>${senha}</strong>
+                    </h3>
+                    <p style="font-size: 11pt;">
+                      você pode acessar o sistema agora ou redefinir sua senha clicando nos botões abaixo.
+                    </p>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <td align="center">
+              <table width="600" cellpadding="0" cellspacing="30" border="0">
+                <tr>
+                  <td align="center">
+                    <a href="https://servicos.spurbanismo.sp.gov.br/apropriacaohoras/?email=${email}"   >
+                      Acessar sistema
+                    </a>
+                  </td>
+                  <td align="center">
+                    <a href="https://servicos.spurbanismo.sp.gov.br/apropriacaohoras/reset?email=${email}%26reset=${senha}" style="">
+                      Redefinir senha
+                    </a>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <td align="center" valign="top">
+              <table width="600" cellpadding="15" cellspacing="0" border="0" class="container" style="margin-top: 2rem;">
+                <tr>
+                  <td align="left" valign="top" bgcolor="#048874">              
+                    <font color="white">
+                      Dúvidas ou problemas? Entre em contato com
+                      <strong>desenvolvimento@spurbanismo.sp.gov.br</strong>
+                    </font>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </body>
+      </html>
       `
     }
   },
--- api/src/index.js
+++ api/src/index.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(middlewares)
+app.use(basePath + '/static', express.static(__dirname + '../../public')) // static files
 
 // create endpoints
 endpoints.forEach(endpoint =>
