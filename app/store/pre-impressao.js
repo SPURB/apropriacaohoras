8370783e77eff1769b678608aed49f593e6dd7f2
m-dantas <mauricio.dantascp@gmail.com>
2020-07-13 13:45:49 -0300

    feat(app): inclui gabarito do arquivo pdf

--- app/store/pre-impressao.js
+++ app/store/pre-impressao.js
@@ -9,6 +9,7 @@ export const state = () => ({
   gruposUsuario: [],
   fasesUsuario: [],
   horas: [],
+  contentForPdf: [],
   fetching: false,
   error: false,
   err: ''
@@ -36,7 +37,9 @@ export const actions = {
     commit('SET', { data: true, key: 'fetching' })
 
     try {
-      const { data } = await Projetos.get(`/[${ids.toString()}]/acoes/agrupar-horas/2000-01-01/${today}${where}`)
+      const { data } = await Projetos.get(
+        `/[${ids.toString()}]/acoes/agrupar-horas/2000-01-01/${today}${where}`
+      )
       commit('SET', { data, key })
       commit('SET', { data: false, key: 'error' })
     } catch (err) {
@@ -51,30 +54,35 @@ export const actions = {
     const projetos = ids.map(id => Projetos.get(`/${id}`))
     promiseAllTable(commit, { promises: projetos, table: 'projetosUsuario' })
   },
-  getGruposUsuario:({ commit }, { ids }) => {
+  getGruposUsuario: ({ commit }, { ids }) => {
     if (!ids || !ids.length) return
     commit('SET', { data: true, key: 'fetching' })
     const grupos = ids.map(id => Grupos.get(`/${id}`))
     promiseAllTable(commit, { promises: grupos, table: 'gruposUsuario' })
   },
-  getFasesUsuario:({ commit }, { ids }) => {
+  getFasesUsuario: ({ commit }, { ids }) => {
     if (!ids || !ids.length) return
     commit('SET', { data: true, key: 'fetching' })
     const grupos = ids.map(id => Fases.get(`?grupo=${id}`))
     promiseAllTable(commit, { promises: grupos, table: 'fasesUsuario' })
   },
-  getHoras:({ commit }, { ids }) => {
+  getHoras: ({ commit }, { ids }) => {
     if (!ids || !ids.length) return
     commit('SET', { data: true, key: 'fetching' })
     const horas = ids.map(id => Horas.get(`?projeto=${id}`))
     promiseAllTable(commit, { promises: horas, table: 'horas' })
   },
+  setContentForPdf: ({ commit }, payload) => {
+    commit('SET', { data: payload, key: 'contentForPdf' })
+  },
   reset: ({ commit }) => commit('RESET')
 }
 
 export const mutations = {
-  SET: (state, { data, key }) => { state[key] = data },
-  RESET: (state) => {
+  SET: (state, { data, key }) => {
+    state[key] = data
+  },
+  RESET: state => {
     state.horasProjetosUsuario = []
     state.fetching = false
   }
