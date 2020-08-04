eeb561334149c7ef32a4446fe7f906a61fc7934c
yubathom <yubathom@gmail.com>
2020-08-04 15:30:07 -0300

    feat(api): mais horas em seeders para desenvolvimento de relatórios

--- api/seeders/006-horas.js
+++ api/seeders/006-horas.js
@@ -40,7 +40,7 @@ module.exports = {
         },
         {
           horas: 1,
-          usuario: 4,
+          usuario: 1,
           extras: 0,
           projeto: 1,
           fase: 1,
@@ -49,6 +49,83 @@ module.exports = {
           createdAt: now,
           updatedAt: now
         },
+        {
+          horas: 2,
+          usuario: 2,
+          extras: 0,
+          projeto: 2,
+          fase: 2,
+          subatividade: 2,
+          dataRefInicio: now,
+          createdAt: now,
+          updatedAt: now
+        },
+        {
+          horas: 3,
+          usuario: 3,
+          extras: 0,
+          projeto: 3,
+          fase: 3,
+          subatividade: 3,
+          dataRefInicio: now,
+          createdAt: now,
+          updatedAt: now
+        },
+        {
+          horas: 4,
+          usuario: 4,
+          extras: 0,
+          projeto: 4,
+          fase: 4,
+          subatividade: 4,
+          dataRefInicio: now,
+          createdAt: now,
+          updatedAt: now
+        },
+        {
+          horas: 1,
+          usuario: 2,
+          extras: 0,
+          projeto: 1,
+          fase: 1,
+          subatividade: 1,
+          dataRefInicio: now,
+          createdAt: now,
+          updatedAt: now
+        },
+        {
+          horas: 2,
+          usuario: 3,
+          extras: 0,
+          projeto: 2,
+          fase: 2,
+          subatividade: 2,
+          dataRefInicio: now,
+          createdAt: now,
+          updatedAt: now
+        },
+        {
+          horas: 3,
+          usuario: 4,
+          extras: 0,
+          projeto: 3,
+          fase: 3,
+          subatividade: 3,
+          dataRefInicio: now,
+          createdAt: now,
+          updatedAt: now
+        },
+        {
+          horas: 4,
+          usuario: 1,
+          extras: 0,
+          projeto: 4,
+          fase: 4,
+          subatividade: 4,
+          dataRefInicio: now,
+          createdAt: now,
+          updatedAt: now
+        }
       ],
       {}
     )
